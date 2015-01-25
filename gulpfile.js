9db6ae040d9ec0cd95dbdcba65547a23e7ec8cd2
Jan Nanista <jan.nanista@gooddata.com>
2015-01-25 21:15:02 +0100

    Added gulp release task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 'use strict';
 
 var fs = require('fs');
+var sys = require('sys');
+var exec = require('child_process').exec;
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var header = require('gulp-header');
+var argv = require('yargs').argv;
 
 var distDir = 'dist';
 var banner = fs.readFileSync('header.txt');
 var pkg = JSON.parse(fs.readFileSync('package.json'));
+var bwr = JSON.parse(fs.readFileSync('bower.json'));
+
+function puts(stdout, stderr) {
+	if (stdout) {
+		sys.puts(stdout);
+	}
+
+	if (stderr) {
+		sys.puts(stdout);
+	}
+}
 
 gulp.task('build', function() {
 	return gulp.src('src/browser/jolokia.js')
@@ -39,4 +53,39 @@ gulp.task('build', function() {
 		.pipe(concat('jolokia.min.js'))
 		.pipe(header(banner, { version: pkg.version }))
 		.pipe(gulp.dest(distDir));
+});
+
+gulp.task('release', ['build'], function(done) {
+	var version = argv.v || argv.version;
+	if (!version || !version.match(/\d+\.\d+\.\d+/)) {
+		throw new Error('Invalid version number: ' + version);
+	}
+
+	bwr.version = pkg.version = version;
+
+	fs.writeFileSync('package.json', JSON.stringify(pkg, null, '  '));
+	fs.writeFileSync('bower.json', JSON.stringify(bwr, null, '  '));
+
+	var semver = 'v' + version,
+		message = '"Release ' + version + '"',
+		commitAndTag = 'git add . && git commit -m ' + message + ' && git tag -a ' + semver + ' -m ' + message,
+		publishAndPush = 'npm publish && git push origin head --tags';
+
+	exec(commitAndTag, function(error, stdout, stderr) {
+		puts(stdout, stderr);
+
+		if (error) {
+			throw new Error('Could not add git semver tag');
+		}
+
+		exec(publishAndPush, function(error, stdout, stderr) {
+			puts(stdout, stderr);
+
+			if (error) {
+				throw new Error('Could not publish to NPM and/or push to repo');
+			}
+
+			done();
+		});
+	});
 });
